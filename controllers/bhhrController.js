5d1b097c8a0ee93df00e0b9d52f0cf74bf5a842d
FrancisBaah <baahfrancis88@gmail.com>
2024-05-23 16:43:25 +0400

    --

--- controllers/bhhrController.js
+++ controllers/bhhrController.js
@@ -9,5 +9,29 @@ const createJob = asyncHandler(async (req, res) => {
   const job = await bhhrModel.create({ title, type, location, length, salary });
   res.status(200).json(job);
 });
+const EditJob = asyncHandler(async (req, res) => {
+  const { title, type, location, length, salary } = req.body;
+  //Check whether job exist
+  const existingJob = await bhhrModel.findById(req.params.id);
+  if (!existingJob) {
+    res.status(400);
+    throw new Error("Job does not exit");
+  }
+  const job = await bhhrModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(job);
+});
+const DeleteJob = asyncHandler(async (req, res) => {
+  const { title, type, location, length, salary } = req.body;
+  //Check whether job exist
+  const existingJob = await bhhrModel.findById(req.params.id);
+  if (!existingJob) {
+    res.status(400);
+    throw new Error("Job does not exit");
+  }
+  await existingJob.deleteOne();
+  res.status(200).json({ id: req.params.id });
+});
 
-module.exports = { createJob, getJobs };
+module.exports = { createJob, getJobs, EditJob, DeleteJob };
--- routes/bhhrRoute.js
+++ routes/bhhrRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { createJob, getJobs } = require("../controllers/bhhrController");
+const {
+  createJob,
+  getJobs,
+  EditJob,
+  DeleteJob,
+} = require("../controllers/bhhrController");
 
 const router = express.Router();
 
 router.route("/").get(getJobs).post(createJob);
+router.route("/:id").put(EditJob).delete(DeleteJob);
 
 module.exports = router;
