0a2c074417bf878e2c36a04d3415368eb9c491b0
FrancisBaah <baahfrancis88@gmail.com>
2024-05-23 10:34:51 +0400

    --

--- models/EmployeeModel.js
+++ models/EmployeeModel.js
@@ -1,33 +1,14 @@
 const mongoose = require("mongoose");
-const Counter = require("./counter");
 
 const employeeSchema = mongoose.Schema(
   {
-    id: {
-      type: Number,
-      unique: true,
-    },
     name: {
       type: String,
-      required: [true, "Please add name"],
+      require: [true, "Please add name"],
     },
     supervisor: String,
   },
   { timestamps: true }
 );
-
-// Pre-save hook to increment the id
-employeeSchema.pre("save", async function (next) {
-  const doc = this;
-  if (doc.isNew) {
-    const counter = await Counter.findByIdAndUpdate(
-      { _id: "id" },
-      { $inc: { seq: 1 } },
-      { new: true, upsert: true }
-    );
-    doc.id = counter.seq;
-  }
-  next();
-});
 
 module.exports = mongoose.model("Employee", employeeSchema);
--- models/counter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const counterSchema = mongoose.Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model("Counter", counterSchema);
